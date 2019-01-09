5607e60e335ac1b03650665580ced6b5594fbea2
Cole Marsteller and Daniel Bennett <daniel@>
2019-01-09 14:15:53 -0800

    add battle function

--- src/battle.js
+++ src/battle.js
@@ -4,13 +4,33 @@ export class Battle {
     this.enemy = enemy;
   }
 
-  function battle() {
+  attack() {
+    let player = this.player;
+    let enemy = this.enemy;
     let playerAttack = setInterval(function() {
-      while (this.enemy.dead === false && this.player.dead === false) {
-        this.enemy.death();
-        this.player.death();
-        this.enemy.health -= this.player.attack;
+      enemy.death();
+      player.death();
+      if (enemy.dead === true || player.dead === true) {
+
+        clearInterval(playerAttack)
+      } else {
+        enemy.health -= player.attack;
+        console.log('player hit');
       }
-    }(5000-(this.player.speed*400)));
+    }, (5000-(player.speed*400)));
+
+    let enemyAttack = setInterval(function() {
+      enemy.death();
+      player.death();
+      if (enemy.dead === true || player.dead === true) {
+
+        clearInterval(enemyAttack)
+      } else {
+        player.health -= enemy.attack;
+        console.log('enemy hit');
+      }
+    }, (5000-(enemy.speed*400)));
+
   }
+
 }
--- src/main.js
+++ src/main.js
@@ -13,11 +13,13 @@ $(document).ready(function() {
   let test_knight = new Knight();
   let test_wizard = new Wizard();
   let test_archer = new Archer();
+  let battle = new Battle(test_archer, test_monster);
   test_knight.weaponCheck();
   test_wizard.weaponCheck();
   test_archer.weaponCheck();
+  battle.attack()
   console.log(test_monster);
-  console.log(test_archer);
-  console.log(test_wizard);
-  console.log(test_knight);
+  // console.log(test_archer);
+  // console.log(test_wizard);
+  // console.log(test_knight);
 });
